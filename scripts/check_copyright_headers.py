c30515293735dc291b7966dc4ee401e7c13b741f
Jeffrey James <lobotmcj@gmail.com>
2020-09-29 20:46:57 -0400

    chore(dev-ux): improve developer tooling
    * add bump2version and commitizen
    * pre-commit: add hook to check commit messages (cz check)
    * pre-commit: make push a default stage
    * Makefile: prevent accidental rm of venv
    
    Signed-off-by: Jeffrey James <lobotmcj@gmail.com>

--- scripts/check_copyright_headers.py
+++ scripts/check_copyright_headers.py
@@ -8,6 +8,7 @@
 import re
 import sys
 from pathlib import Path
+from typing import List
 
 from pre_commit import output
 
@@ -18,6 +19,27 @@ EXPECTED_HEADER = (
 )
 
 
+def get_header_lines(fpath: Path) -> List[str]:
+    h_lines = []
+    with fpath.open() as f:
+        for line in f:
+            # skip shebang
+            if re.match(r'#![/"].*', line):
+                continue
+            # skip encoding declaration
+            elif re.search(r"coding[=:]\s*([-\w.]+)", line):
+                continue
+            # skip blank lines
+            elif line.strip() in ["", "#"]:
+                continue
+            else:
+                h_lines.append(line)
+
+            if len(h_lines) == 3:
+                break
+    return h_lines
+
+
 if __name__ == "__main__":
     retv = 0
     for filename in sys.argv[1:]:
@@ -25,32 +47,15 @@ if __name__ == "__main__":
         if not filepath.is_file():
             continue
 
-        lines_to_check = []
-        with filepath.open() as f:
-            for line in f:
-                # skip shebang
-                if re.match(r'#![/"].*', line):
-                    continue
-                # skip encoding declaration
-                elif re.search(r"coding[=:]\s*([-\w.]+)", line):
-                    continue
-                # skip blank lines
-                elif line.strip() in ["", "#"]:
-                    continue
-                else:
-                    lines_to_check.append(line)
-
-                if len(lines_to_check) == 3:
-                    break
-
-        if len(lines_to_check) < 3:
+        header_lines = get_header_lines(filepath)
+        if len(header_lines) < 3:
             retv |= 1
             output.write_line(f"{filename}: (not enough lines)")
         else:
-            header = "".join(lines_to_check)
+            header = "".join(header_lines)
             if header != EXPECTED_HEADER:
                 retv |= 1
-                output.write(f"{filename}:\n  {'  '.join(lines_to_check)}")
+                output.write(f"{filename}:\n  {'  '.join(header_lines)}")
             else:
                 retv |= 0
     sys.exit(retv)
